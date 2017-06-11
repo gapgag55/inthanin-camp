3d26ebd2e3cbb166739aaed7ab0ec1c1a1ce5075
Sarayut <imkopkap@gmail.com>
2017-06-11 17:18:01 +0700

    image

--- pages/index.js
+++ pages/index.js
@@ -15,6 +15,11 @@ class App extends Component {
           <title>Inthanin Guild Camp #1</title>
           <link rel="shortcut icon" type="image/png" href="static/images/favicon.png"/>
           <style dangerouslySetInnerHTML={{ __html: stylesheet }} />
+          <meta property="og:url"                content="http://www.inthanincamp.xyz/" />
+          <meta property="og:type"               content="article" />
+          <meta property="og:title"              content="เปิดรับสมัคร Staff ค่ายอินทนิลไกด์แคมป์ #1" />
+          <meta property="og:description"        content="ตอนนี้กำลังเปิดรับสมัครพี่ๆช่วยงานค่ายอินทนิลไกด์แคมป์ ค่ายสานฝันสำหรับน้องๆโรงเรียนเทศบาลเฉลิมพระเกียรติฯสมเด็จพระเทพรัตนราชสุดาสยามบรมราชกุมารี" />
+          <meta property="og:image"              content="http://static01.nyt.com/images/2015/02/19/arts/international/19iht-btnumbers19A/19iht-btnumbers19A-facebookJumbo-v2.jpg" />
         </Head>
         <Hero />
         <Form />
